2128c4d2e6181b8eaa1663c2941f3d66f9b62180
zolchip0 <zolchip0@gmail.com>
2026-06-18 12:16:18 +0300

    start report router

--- routers/report_routes.py
+++ routers/report_routes.py
@@ -1,3 +1,30 @@
 from fastapi import APIRouter,HTTPException
 from database.mission_db import Mission
+mission = Mission()
 router = APIRouter()
+@router.get("/summary")
+def general_report():
+    """{
+        "active_agents_count": 0,
+        "total_missions": 0,
+        "open_missions": 0,
+        "completed_missions": 0,
+        "failed_missions": 0,
+        "critical_missions": 0
+        }"""
+@router.get("/missions-by-status")
+def missions_by_status():
+    """{
+        "open": 5,
+        "in_progress": 3,
+        "completed": 12,
+        "failed": 1,
+        "canceled": 2
+        }
+        """
+@router.get("/top-agent")
+def top_agent():
+    data = mission.get_top_agents()
+    return {"message":data}
+
+      
